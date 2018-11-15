67594b4196c5f28265888a207156213fb78f67c4
MHO <mauricio.hdez.o.10@gmail.com>
2018-11-14 19:45:55 -0600

    Preparing for Navigation

--- app/src/main/java/com/raywenderlich/android/imet/ui/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/imet/ui/MainActivity.kt
@@ -35,13 +35,22 @@ package com.raywenderlich.android.imet.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.raywenderlich.android.imet.R
 
 class MainActivity : AppCompatActivity() {
 
+  private lateinit var navigationController: NavController
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    navigationController = findNavController(R.id.navigationHostFragment)
+    NavigationUI.setupActionBarWithNavController(this, navigationController)
   }
 
+  override fun onSupportNavigateUp() = navigationController.navigateUp()
 }
